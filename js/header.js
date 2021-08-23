8a7891fae060674c4df78b6840c5b5e7be06fc83
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2021-08-23 22:03:53 +0100

    Update 23/8/21
    Fixed mobile menu and scroll effects problems with the page.
    
    Problem:
    Each time the hambugger menu was clicked, the mobile drop down menu appeared and the hambugger menu crossed and became an X. But when you clicked the X to close the drop down menu, the page would scroll back to the top.
    
    Solution:
    Added an event listener to the div containing the mobile menu, and closed it on click. This prevents the need to close the menu in the middle of the page - which ordinarily would cause it to scroll back to the top.

--- js/header.js
+++ js/header.js
@@ -1,3 +1,4 @@
+//Grab the hambugger menu icon and 
 const mobileToggle = document.getElementById('mobile_toggle');
 const mobileMenu = document.querySelector('.mobile_menu');
 
@@ -5,9 +6,16 @@ const mobileMenu = document.querySelector('.mobile_menu');
 
 mobileToggle.addEventListener('click', function() {
     if(mobileToggle.classList.contains('opened')) {
-        mobileToggle.classList.remove('opened');
-        mobileMenu.classList.remove('menu_drop_down')
+        mobileToggle.classList.remove('opened');  // close X - hambugger menu
+        mobileMenu.classList.remove('menu_drop_down') //close dropdown menu
     }
     else {mobileToggle.classList.add('opened');
          mobileMenu.classList.add('menu_drop_down')}
 });
+
+//close mobile menu when we click on any of the mobile menu links
+mobileMenu.addEventListener('click', closeMobileMenu);
+function closeMobileMenu() {
+    mobileToggle.classList.remove('opened');
+    mobileMenu.classList.remove('menu_drop_down')
+}
